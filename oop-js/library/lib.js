854fe0ccdaf6fb0f02988c617d7b03df7b909075
jaimin patel <jaimin9999@gmail.com>
2014-02-17 15:16:36 -0500

    added few more

---
+++ oop-js/library/lib.js
@@ -0,0 +1,19 @@
+// based on instruction http://net.tutsplus.com/tutorials/javascript-ajax/build-your-first-javascript-library/
+
+window.dome = (function(){
+
+	// constructor
+	function Dome (els){
+
+	}
+
+	// object, actual library object
+	var dome = {
+		get: function(){
+
+		}
+	};
+
+	return dome;
+
+}());
